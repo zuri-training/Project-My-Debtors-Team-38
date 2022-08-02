acd257c04403984a6604fa1581e8da3010cf437b
jpphilips <78326013+jpphilips@users.noreply.github.com>
2022-08-02 23:43:41 +0100

    Update models.py

--- SDM/posts/models.py
+++ SDM/posts/models.py
@@ -5,19 +5,19 @@ from datetime import datetime
 
 
 class Contend(models.Model):
-    id = models.IntegerField(primary_key=True)
-    debt_id = models.IntegerField()
-    guardian_id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)#not needed django does it by default
+    debt_id = models.IntegerField() #foriegn key
+    guardian_id = models.IntegerField() #foriegn key
     made_payment = models.BooleanField(default=False)
-    receipt = models.ImageField()
+    receipt = models.ImageField() #think we should use charfield for links in production shar
     created_at = models.DateTimeField(default=datetime.now)
 
 
 
 class Comments(models.Model):
-    debt_id = models.IntegerField()
-    school_id = models.IntegerField()
-    id = models.IntegerField(primary_key=True)
+    debt_id = models.IntegerField()#foriegn key
+    school_id = models.IntegerField()#foriegn key
+    id = models.IntegerField(primary_key=True)#not needed django does it by default
     body = models.TextField()
     created_at = models.DateTimeField(datetime.now)
     updated_at = models.DateTimeField(datetime.now)
