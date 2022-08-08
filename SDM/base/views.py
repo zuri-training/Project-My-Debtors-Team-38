764877b476fc73da5cca9a8f4779ef69a30929e6
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2022-08-08 14:19:36 +0100

    added school_reg_page() to SDM/base/views.py as its absence was crashing the server

--- SDM/base/views.py
+++ SDM/base/views.py
@@ -10,3 +10,6 @@ def school_list_page(request):
 
 def contact_us_page(request):
     return render(request, 'base/contact_us.html')
+
+def school_reg_page(request):
+    return render(request, 'base/school_reg_with_us.html')
