43fad66b9a05ef13ee50936669ff99a3b231864b
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2022-08-08 16:51:05 +0100

    created message app model

--- SDM/message/models.py
+++ SDM/message/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
+from accounts.models import School
+from posts.models import Debt
 
 # Create your models here.
+class SchoolMessage(models.Model):
+  school_id = models.ForeignKey(School, on_delete=models.CASCADE)
+  receiver_id = models.IntegerField()
+  debt_id = models.ForeignKey(Debt, on_delete=models.CASCADE)
+  body = models.TextField()
+  created_at = models.DateTimeField(default=datetime.now)
+
