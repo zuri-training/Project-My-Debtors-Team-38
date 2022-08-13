3bf5694e4ff5b785c64e3d4beb41653ebb3328f8
Christian Tom <christiantom40@gmail.com>
2022-08-13 17:33:23 +0100

    admin.py for posts app

--- SDM/posts/admin.py
+++ SDM/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from posts.models import Post, Contend, Comment
+
+admin.site.register(Post)
+admin.site.register(Contend)
+admin.site.register(Comment)
+
 
 # Register your models here.
